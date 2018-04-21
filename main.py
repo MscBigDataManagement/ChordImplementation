03d3d320995a0bc8bc543446a9199bf2d552acc4
thanasis karidis <than.karidis@gmail.com>
2018-04-21 22:58:17 +0300

    create node with hashed ip

--- main.py
+++ main.py
@@ -1,28 +1,59 @@
-import sys
+import hashlib
+import random
+
+
 
 class Node:
 	"""A class that creates a Node"""
-	def __init__(self, ip, port):
-		self.ip = ip
-		self.port = port
+	predecessor = 0
+	successor = 0
+	finger_table =[]
+
+	def __init__(self,nodes):
+		self.ip = str(random.randint(0, 255)) + '.' + str(random.randint(0, 255)) + '.' + str(random.randint(0, 255)) + '.' + str(random.randint(0, 255)) + ":" + str(random.randint(0, 65535))
+		self.hashed_ip = hash(nodes)
+
+	def __predeccessor__(self):
+		Node.finger_table = 1
+
+	def successor(self):
+		pass
+
+	def finger_table(self):
+		pass
+
+	def hash(self,nodes):
+		hash_object = hashlib.sha1(self.ip)
+		return int(hash_object.hexdigest(), 16) % ((2 ** nodes) - 1)
 
 def main():
 	#ask user to give the number of nodes
-	# try:
-	user_input = int(input("Enter the number of nodes n\n"))
+	nodes = int(input("Enter the number of nodes n\n"))
+	requests = int(input("Enter the number requests \n"))
 
-	# the nodes that have been created will be added to a list which
-	# shows all the alive nodes (In this project we suppose that every
-	# node can not die)
-	alive = []
+	hashed_req = hashing(nodes, requests)
+	# print hashed_req
 
-	x = Node(1, 2)
-	print x.ip, x.port
+	x = Node(nodes)
+	print x.ip
+	print x.hashed_ip
 
-	# except ValueError:
-	# 	print 'Error %s' % e
-	# 	sys.exit(1)
+def create_node():
+
+
+def hashing(nodes, requests):
+	hash_list = []
+	with open('/Users/thanasiskaridis/Desktop/BigDataManagement/ChordImplementation/filenames.txt') as f:
+		lines = random.sample(f.readlines(), requests)
+		for line in lines:
+			hash_object = hashlib.sha1(line)
+			hash_key = int(hash_object.hexdigest(), 16) % ((2 ** nodes) - 1)
+			hash_list.append(hash_key)
+	return hash_list
+
 
 if __name__ == "__main__":
 	main()
+
+
 
