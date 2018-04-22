2db0c4433d09afc2465815df0d9968b8363b4026
thanasis karidis <than.karidis@gmail.com>
2018-04-22 22:11:12 +0300

    fingerTable not fixed

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class Node:
 		hash_object = hashlib.sha1(self.ip)        
 		self.hashed_ip = int(hash_object.hexdigest(), 16) % ((2 ** nodes) - 1)
 		self.finger_table = []
+		self.responsible = ()
 
 	def predecessor_successor(self,keylist):
 		for i in keylist:
@@ -24,7 +25,7 @@ class Node:
 
 	def fill_finger_table(self, nodes, keylist):
 		next_node = 0
-		for i in range(nodes):
+		for i in range(0, nodes):
 			id = self.hashed_ip + 2 ** i
 			if id >= (2 ** nodes):
 				id2 = id - (2 ** nodes)
@@ -39,32 +40,37 @@ class Node:
 			record = (id, next_node)
 			self.finger_table.append(record)
 
+	def responsibility(self):
+		self.responsible = (self.predecessor+1, self.hashed_ip)
+		return self.responsible
+
 def main():
 	#ask user to give the number of nodes
 	nodes = int(input("Enter the number of nodes n\n"))
 	requests = int(input("Enter the number requests \n"))
-
-	hashed_req = hashing(nodes, requests)
-
-
 	keylist, dict = create_nodes(nodes)
 	print keylist
-
+	# print dict
 	for i in keylist:
 		dict[i].predecessor_successor(keylist)
+		dict[i].responsibility()
+		print dict[i].responsible
 		dict[i].fill_finger_table(nodes,keylist)
-		print dict[i].finger_table
+	hashed_req = hashing(nodes, requests, keylist)
+	# print hashed_req
+	for item in hashed_req:
+		end = lookup(item[0], item[2], dict)
 
 
-
-def hashing(nodes, requests):
+def hashing(nodes, requests, keylist):
 	hash_list = []
-	with open('filenames.txt') as f:
+	with open('/Users/thanasiskaridis/Desktop/BigDataManagement/ChordImplementation/filenames.txt') as f:
 		lines = random.sample(f.readlines(), requests)
 		for line in lines:
 			hash_object = hashlib.sha1(line)
 			hash_key = int(hash_object.hexdigest(), 16) % ((2 ** nodes) - 1)
-			hash_list.append(hash_key)
+			hash_tuple = (hash_key, line.rstrip('\n'), random.choice(keylist))
+			hash_list.append(hash_tuple)
 	return hash_list
 
 def create_nodes(nodes):
@@ -76,6 +82,30 @@ def create_nodes(nodes):
 	keylist.sort()
 	return keylist, dict
 
+def lookup(request, start,dict):
+	# print dict[start].finger_table
+	if request <= start:
+		if dict[start].responsible[0] <= request <= dict[start].responsible[1]:
+			return start
+		else:
+			for k in dict[start].finger_table:
+				if k[1] <= request:
+					new_start = k[1]
+					return lookup(request, new_start, dict)
+			new_start = dict[start].finger_table[-1][1]
+			return lookup(request, new_start, dict)
+	elif request > dict[start].finger_table[-1][0]:
+		new_start = dict[start].finger_table[-1][1]
+		return lookup(request, new_start, dict)
+	else:
+		for i in dict[start].finger_table:
+			if request == i[0]:
+				if i[1].responsible[0] <= request <= i[1].responsible[1]:
+					return i[1]
+			elif request > i[0]:
+				ind = i.index(i[0])
+				new_start = dict[start].finger_table[ind - 1][1]
+				return lookup(request, new_start, dict)
 
 if __name__ == "__main__":
 	main()
